03897209237457294ecb5923ca2181d97149c4ab
Ronny Trommer <ronny@opennms.org>
2019-06-19 17:44:33 +0200

    NMS-12120: Add health wrapper for Minion and Sentinel

---
+++ opennms-container/sentinel/assets/health.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+SENTINEL_HOME="/opt/sentinel"
+HEALTH_TEST="Everything is awesome"
+
+# Wrap health check for container health checks to 0 and 1
+#
+# Reference: https://docs.docker.com/engine/reference/builder/
+# The command’s exit status indicates the health status of the container. The possible values are:
+#
+#   0: success - the container is healthy and ready for use
+#   1: unhealthy - the container is not working correctly
+#   2: reserved - do not use this exit code
+if ${SENTINEL_HOME}/bin/client health:check | grep "${HEALTH_TEST}"; then
+  echo "Health check success."
+  exit 0
+else
+  echo "Health check failed."
+  exit 1
+fi
